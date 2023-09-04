d5c61809be31b3e5bed2288d803e5bbca7c07d87
Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>
2023-09-04 18:29:39 +0100

    Add endpoint for getting a specific container using it's ID
    Signed-off-by: Chihurumnaya Ibiam <ibiamchihurumnaya@gmail.com>

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ var containers []containerDetails
 func main() {
 	router := gin.Default()
 	router.GET("/containers", getContainers)
+	router.GET("/containers/:id", getContainerByID)
 	router.POST("/containers", postContainers)
 
 	router.Run("localhost:8080")
@@ -42,4 +43,17 @@ func postContainers(c *gin.Context) {
 
 	containers = append(containers, details)
 	c.IndentedJSON(http.StatusCreated, details)
+}
+
+func getContainerByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, v := range containers {
+		if v.ID == id {
+			c.IndentedJSON(http.StatusOK, v)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "container not found"})
 }
